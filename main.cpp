4aab2804672c7f3abbde809632f4c6d07b4b39c4
rodsmade <rodrigogregoldo@gmail.com>
2023-03-24 14:24:25 -0300

    added copy constructor test

--- main.cpp
+++ main.cpp
@@ -4,36 +4,58 @@ void test_vector_creation() {
     unsigned int intendedSize = 42;
     ft::Vector testVector(intendedSize);
 
-    // Testing Size on Creation
-    std::cout << "Size on creation: ";
-    if (testVector.size() == intendedSize)
-        std::cout << BOLD LIGHT_GREEN "OK" RESET;
-    else
-        std::cout << BOLD LIGHT_RED "NOK" RESET;
-    std::cout << std::endl;
-
-    // Testing Initialisation of values
-    std::cout << "Initialisation values: ";
-    for (unsigned int i = 0; i < testVector.size(); i++) {
-
-        if (testVector.get(i) != 0)
+    {  // Testing Size on Creation
+        std::cout << "Size on creation: ";
+        if (testVector.size() == intendedSize)
+            std::cout << BOLD LIGHT_GREEN "OK" RESET;
+        else
             std::cout << BOLD LIGHT_RED "NOK" RESET;
+        std::cout << std::endl;
     }
-    std::cout << BOLD LIGHT_GREEN "OK" RESET;
-    std::cout << std::endl;
-
-    // Testing Valid set() operation at valid index
-    std::cout << "Setting value at valid index: ";
-
-    double designatedValue = 42.0;
-    unsigned int designatedIndex = 12;
-    
-    testVector.set(designatedIndex, designatedValue);
-    if (testVector.get(designatedIndex) == designatedValue)
+    {  // Testing Initialisation of values
+        std::cout << "Initialisation values: ";
+        for (unsigned int i = 0; i < testVector.size(); i++) {
+            if (testVector.get(i) != 0)
+                std::cout << BOLD LIGHT_RED "NOK" RESET;
+        }
         std::cout << BOLD LIGHT_GREEN "OK" RESET;
-    else
-        std::cout << BOLD LIGHT_RED "NOK" RESET;
-    std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    {  // Testing Valid set() operation at valid index
+        std::cout << "Setting value at valid index: ";
+
+        double designatedValue = 42.0;
+        unsigned int designatedIndex = 12;
+
+        testVector.set(designatedIndex, designatedValue);
+        if (testVector.get(designatedIndex) == designatedValue)
+            std::cout << BOLD LIGHT_GREEN "OK" RESET;
+        else
+            std::cout << BOLD LIGHT_RED "NOK" RESET;
+        std::cout << std::endl;
+    }
+    { // Testing copy construction
+        ft::Vector copyVector(testVector);
+
+        std::cout << "Copy-construction: ";
+        bool sizeTest = true;
+        bool elementsTest = true;
+        
+        sizeTest = copyVector.size() == testVector.size();
+        
+        for (unsigned int i = 0; i < copyVector.size(); i++) {
+            if (copyVector.get(i) != testVector.get(i)) {
+                elementsTest = false;
+                break;
+            }
+        }
+
+        if (sizeTest && elementsTest)
+            std::cout << BOLD LIGHT_GREEN "OK" RESET;
+        else
+            std::cout << BOLD LIGHT_RED "NOK" RESET;
+        std::cout << std::endl;
+    }
 }
 
 int main() {
