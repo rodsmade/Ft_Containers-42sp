45a5ead4d77138f8be57b402eba094620f6408be
rodsmade <rodrigogregoldo@gmail.com>
2023-04-14 17:18:46 -0300

    testing the differences between accessing via [] and via at()

--- POCs/vector_subscript_operator.cpp
+++ POCs/vector_subscript_operator.cpp
@@ -6,16 +6,17 @@ int main() {
 
     vetor[0] = 43;
     // vetor[50] = 44;
-    vetor[5] = 45;
 
     for (std::vector<int>::iterator it = vetor.begin(); it != vetor.end(); it++)
         std::cout << *it << std::endl;
 
     int i = -1;
     while (std::cin >> i && i !=-1)
-        // try {
+        try {
             std::cout << "v[" << i << "]==" << vetor.at(i) << "\n";
-        // } catch (const std::exception &e) {
-            // std::cout << "bad index: " << i << "\n";
-        // }
+        } catch (const std::exception &e) {
+            std::cout << "bad index: " << i << "\n";
+        }
+
+    // conclusão: o at estoura uma exceção se o acesso for inválido. o [] não, é totalmente desprotegido (e perigoso).
 }
