04497ac32b09ce8528d78691c4411bb362d2deaf
rodsmade <rodrigogregoldo@gmail.com>
2023-05-07 18:33:32 -0300

    added new test for reserve to check if it works when vector is initially zeroed

--- tests/test_reserve.cpp
+++ tests/test_reserve.cpp
@@ -15,4 +15,12 @@ void test_reserve(void) {
     stdVector.reserve(5);
     assert(ftVector.size() == stdVector.size(), "Size check against std::vector after resizing to a lower value");
     assert(ftVector.capacity() == stdVector.capacity(), "Capacity check against std::vector after resizing to a lower value");
+
+    ft::vector<int> ftEmptyVector;
+    std::vector<int> stdEmptyVector;
+
+    ftEmptyVector.reserve(42);
+    stdEmptyVector.reserve(42);
+    assert(ftEmptyVector.size() == stdEmptyVector.size(), "Size check against std::vector when resizing an empty vector");
+    assert(ftEmptyVector.capacity() == stdEmptyVector.capacity(), "Capacity check against std::vector when resizing an empty vector");
 }
