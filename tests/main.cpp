f602cc9e62ea106145c8eb536f102967d632d89a
rodsmade <rodrigogregoldo@gmail.com>
2023-05-07 15:53:22 -0300

    finished imlpementation of all three specializations of insert() and respective tests. closes #30

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,27 +1,27 @@
 #include "test_functions_header.hpp"
 
 int main() {
-    // test_typedefs();
-    // test_vector_construction();
-    // test_max_size();
-    // test_size_and_capacity();
-    // test_resize();
-    // test_reserve();
-    // test_assignment_operator();
-    // test_cleanup();
-    // test_emptiness();
-    // test_push_back();
-    // test_access();
-    // test_pop_back();
-    // test_back();
-    // test_front();
-    // test_data();
-    // test_swap();
-    // test_assign();
-    // test_allocator();
-    // test_operator_overloads();
-    // test_iterators();
-    // test_erase();
+    test_typedefs();
+    test_vector_construction();
+    test_max_size();
+    test_size_and_capacity();
+    test_resize();
+    test_reserve();
+    test_assignment_operator();
+    test_cleanup();
+    test_emptiness();
+    test_push_back();
+    test_access();
+    test_pop_back();
+    test_back();
+    test_front();
+    test_data();
+    test_swap();
+    test_assign();
+    test_allocator();
+    test_operator_overloads();
+    test_iterators();
+    test_erase();
     test_insertion();
     return 0;
 }
--- tests/test_insertion.cpp
+++ tests/test_insertion.cpp
@@ -11,16 +11,18 @@ void test_insertion(void) {
     std::cout << "\n=== Test insertion methods ( insert() ) ===" << std::endl;
     {
         std::cout << "  Single element insert ( insert(const_iterator pos, const_reference value) )" << std::endl;
-        ft::vector<int> ftVector(10);
+        std::cout << "    Insertion size within current capacity (no internal reserve() needed)" << std::endl;
+        ft::vector<int> ftVector;
+
         for (int i = 0; i < 10; i++)
-            ftVector.at(i) = i + 1;
+            ftVector.push_back(i + 1);
 
         ft::vector<int>::size_type oldSize = ftVector.size();
 
         ftVector.insert(ftVector.begin() + 5, 42);
 
-        assert(ftVector.size() == oldSize + 1 &&
-            ftVector.at(0) == 1 &&
+        assert(ftVector.size() == oldSize + 1, "Size check for insertion in arbitrary position");
+        assert(ftVector.at(0) == 1 &&
             ftVector.at(1) == 2 &&
             ftVector.at(2) == 3 &&
             ftVector.at(3) == 4 &&
@@ -31,13 +33,13 @@ void test_insertion(void) {
             ftVector.at(8) == 8 &&
             ftVector.at(9) == 9 &&
             ftVector.at(10) == 10
-            , "Consistency check");
+            , "Consistency check for insertion in arbitrary position");
 
         oldSize = ftVector.size();
         ftVector.insert(ftVector.begin(), 21);
 
-        assert(ftVector.size() == oldSize + 1 &&
-            ftVector.at(0) == 21 &&
+        assert(ftVector.size() == oldSize + 1, "Size check for insertion in the beginning of vector");
+        assert(ftVector.at(0) == 21 &&
             ftVector.at(1) == 1 &&
             ftVector.at(2) == 2 &&
             ftVector.at(3) == 3 &&
@@ -49,13 +51,13 @@ void test_insertion(void) {
             ftVector.at(9) == 8 &&
             ftVector.at(10) == 9 &&
             ftVector.at(11) == 10
-            , "Consistency check inserting element in the beginning of vector");
+            , "Consistency check for insertion in the beginning of vector");
 
         oldSize = ftVector.size();
         ftVector.insert(ftVector.end(), 84);
 
-        assert(ftVector.size() == oldSize + 1 &&
-            ftVector.at(0) == 21 &&
+        assert(ftVector.size() == oldSize + 1, "Size check for insertion at the end of vector");
+        assert(ftVector.at(0) == 21 &&
             ftVector.at(1) == 1 &&
             ftVector.at(2) == 2 &&
             ftVector.at(3) == 3 &&
@@ -68,21 +70,85 @@ void test_insertion(void) {
             ftVector.at(10) == 9 &&
             ftVector.at(11) == 10 &&
             ftVector.at(12) == 84
-            , "Consistency check inserting element in the end of vector");
+            , "Consistency check for insertion at the end of vector");
+    }
+    {
+        std::cout << "    Insertion size greater than current capacity (internal reserve() needed)" << std::endl;
+        ft::vector<int> ftVector(10);
+        for (int i = 0; i < 10; i++)
+            ftVector.at(i) = i + 1;
+
+        ft::vector<int>::size_type oldSize = ftVector.size();
+
+        ftVector.insert(ftVector.begin() + 5, 42);
+
+        assert(ftVector.size() == oldSize + 1, "Size check for insertion in arbitrary position");
+        assert(ftVector.at(0) == 1 &&
+            ftVector.at(1) == 2 &&
+            ftVector.at(2) == 3 &&
+            ftVector.at(3) == 4 &&
+            ftVector.at(4) == 5 &&
+            ftVector.at(5) == 42 &&
+            ftVector.at(6) == 6 &&
+            ftVector.at(7) == 7 &&
+            ftVector.at(8) == 8 &&
+            ftVector.at(9) == 9 &&
+            ftVector.at(10) == 10
+            , "Consistency check for insertion in arbitrary position");
+
+        oldSize = ftVector.size();
+        ftVector.insert(ftVector.begin(), 21);
+
+        assert(ftVector.size() == oldSize + 1, "Size check for insertion in the beginning of vector");
+        assert(ftVector.at(0) == 21 &&
+            ftVector.at(1) == 1 &&
+            ftVector.at(2) == 2 &&
+            ftVector.at(3) == 3 &&
+            ftVector.at(4) == 4 &&
+            ftVector.at(5) == 5 &&
+            ftVector.at(6) == 42 &&
+            ftVector.at(7) == 6 &&
+            ftVector.at(8) == 7 &&
+            ftVector.at(9) == 8 &&
+            ftVector.at(10) == 9 &&
+            ftVector.at(11) == 10
+            , "Consistency check for insertion in the beginning of vector");
+
+        oldSize = ftVector.size();
+        ftVector.insert(ftVector.end(), 84);
+
+        assert(ftVector.size() == oldSize + 1, "Size check for insertion at the end of vector");
+        assert(ftVector.at(0) == 21 &&
+            ftVector.at(1) == 1 &&
+            ftVector.at(2) == 2 &&
+            ftVector.at(3) == 3 &&
+            ftVector.at(4) == 4 &&
+            ftVector.at(5) == 5 &&
+            ftVector.at(6) == 42 &&
+            ftVector.at(7) == 6 &&
+            ftVector.at(8) == 7 &&
+            ftVector.at(9) == 8 &&
+            ftVector.at(10) == 9 &&
+            ftVector.at(11) == 10 &&
+            ftVector.at(12) == 84
+            , "Consistency check for insertion at the end of vector");
     }
     {
         std::cout << "\n  Fill insert ( insert(const_iterator pos, size_type count, const_reference value) )" << std::endl;
-        ft::vector<int> ftVector;
-        for (int i = 1; i <=10; i++)
-            ftVector.push_back(i);
+        std::cout << "    Insertion size within current capacity (no internal reserve() needed)" << std::endl;
+        ft::vector<int> ftVector(50);
+
+        ftVector.resize(10);
+        for (int i = 0; i < 10; i++)
+            ftVector.at(i) = i + 1;
 
         ft::vector<int>::size_type oldSize = ftVector.size();
         ft::vector<int>::size_type numberOfCopies = 6;
 
         ftVector.insert(ftVector.begin() + 5, numberOfCopies, 42);
 
-        assert(ftVector.size() == oldSize + numberOfCopies &&
-            ftVector.at(0) == 1 &&
+        assert(ftVector.size() == oldSize + numberOfCopies, "Size check for insertion in arbitrary position");
+        assert(ftVector.at(0) == 1 &&
             ftVector.at(1) == 2 &&
             ftVector.at(2) == 3 &&
             ftVector.at(3) == 4 &&
@@ -98,14 +164,14 @@ void test_insertion(void) {
             ftVector.at(13) == 8 &&
             ftVector.at(14) == 9 &&
             ftVector.at(15) == 10
-            , "Consistency check");
+            , "Consistency check for insertion in arbitrary position");
 
         oldSize = ftVector.size();
         numberOfCopies = 3;
         ftVector.insert(ftVector.begin(), numberOfCopies, 21);
 
-        assert(ftVector.size() == oldSize + numberOfCopies &&
-            ftVector.at(0) == 21 &&
+        assert(ftVector.size() == oldSize + numberOfCopies, "Size check for insertion in the beginning of vector");
+        assert(ftVector.at(0) == 21 &&
             ftVector.at(1) == 21 &&
             ftVector.at(2) == 21 &&
             ftVector.at(3) == 1 &&
@@ -124,14 +190,14 @@ void test_insertion(void) {
             ftVector.at(16) == 8 &&
             ftVector.at(17) == 9 &&
             ftVector.at(18) == 10
-            , "Consistency check inserting element in the beginning of vector");
+            , "Consistency check for insertion in the beginning of vector");
 
         oldSize = ftVector.size();
-        numberOfCopies = 3;
+        numberOfCopies = 4;
         ftVector.insert(ftVector.end(), numberOfCopies, 84);
 
-        assert(ftVector.size() == oldSize + numberOfCopies &&
-            ftVector.at(0) == 21 &&
+        assert(ftVector.size() == oldSize + numberOfCopies, "Size check for insertion at the end of vector");
+        assert(ftVector.at(0) == 21 &&
             ftVector.at(1) == 21 &&
             ftVector.at(2) == 21 &&
             ftVector.at(3) == 1 &&
@@ -154,7 +220,94 @@ void test_insertion(void) {
             ftVector.at(20) == 84 &&
             ftVector.at(21) == 84 &&
             ftVector.at(22) == 84
-            , "Consistency check inserting element in the end of vector");
+            , "Consistency check for insertion at the end of vector");
+    }
+    {
+        std::cout << "    Insertion size greater than current capacity (internal reserve() needed)" << std::endl;
+        ft::vector<int> ftVector;
+
+        for (int i = 1; i <= 10; i++)
+            ftVector.push_back(i);
+
+        ft::vector<int>::size_type oldSize = ftVector.size();
+        ft::vector<int>::size_type numberOfCopies = 6;
+
+        ftVector.insert(ftVector.begin() + 5, numberOfCopies, 42);
+
+        assert(ftVector.size() == oldSize + numberOfCopies, "Size check for insertion at arbitrary position");
+        assert(ftVector.at(0) == 1 &&
+            ftVector.at(1) == 2 &&
+            ftVector.at(2) == 3 &&
+            ftVector.at(3) == 4 &&
+            ftVector.at(4) == 5 &&
+            ftVector.at(5) == 42 &&
+            ftVector.at(6) == 42 &&
+            ftVector.at(7) == 42 &&
+            ftVector.at(8) == 42 &&
+            ftVector.at(9) == 42 &&
+            ftVector.at(10) == 42 &&
+            ftVector.at(11) == 6 &&
+            ftVector.at(12) == 7 &&
+            ftVector.at(13) == 8 &&
+            ftVector.at(14) == 9 &&
+            ftVector.at(15) == 10
+            , "Consistency check for insertion at arbitrary position");
+
+        oldSize = ftVector.size();
+        numberOfCopies = 3;
+        ftVector.insert(ftVector.begin(), numberOfCopies, 21);
+
+        assert(ftVector.size() == oldSize + numberOfCopies, "Size check for insertion in the beginning of vector");
+        assert(ftVector.at(0) == 21 &&
+            ftVector.at(1) == 21 &&
+            ftVector.at(2) == 21 &&
+            ftVector.at(3) == 1 &&
+            ftVector.at(4) == 2 &&
+            ftVector.at(5) == 3 &&
+            ftVector.at(6) == 4 &&
+            ftVector.at(7) == 5 &&
+            ftVector.at(8) == 42 &&
+            ftVector.at(9) == 42 &&
+            ftVector.at(10) == 42 &&
+            ftVector.at(11) == 42 &&
+            ftVector.at(12) == 42 &&
+            ftVector.at(13) == 42 &&
+            ftVector.at(14) == 6 &&
+            ftVector.at(15) == 7 &&
+            ftVector.at(16) == 8 &&
+            ftVector.at(17) == 9 &&
+            ftVector.at(18) == 10
+            , "Consistency check for insertion in the beginning of vector");
+
+        oldSize = ftVector.size();
+        numberOfCopies = 4;
+        ftVector.insert(ftVector.end(), numberOfCopies, 84);
+
+        assert(ftVector.size() == oldSize + numberOfCopies, "Size check for insertion at the end of vector");
+        assert(ftVector.at(0) == 21 &&
+            ftVector.at(1) == 21 &&
+            ftVector.at(2) == 21 &&
+            ftVector.at(3) == 1 &&
+            ftVector.at(4) == 2 &&
+            ftVector.at(5) == 3 &&
+            ftVector.at(6) == 4 &&
+            ftVector.at(7) == 5 &&
+            ftVector.at(8) == 42 &&
+            ftVector.at(9) == 42 &&
+            ftVector.at(10) == 42 &&
+            ftVector.at(11) == 42 &&
+            ftVector.at(12) == 42 &&
+            ftVector.at(13) == 42 &&
+            ftVector.at(14) == 6 &&
+            ftVector.at(15) == 7 &&
+            ftVector.at(16) == 8 &&
+            ftVector.at(17) == 9 &&
+            ftVector.at(18) == 10 &&
+            ftVector.at(19) == 84 &&
+            ftVector.at(20) == 84 &&
+            ftVector.at(21) == 84 &&
+            ftVector.at(22) == 84
+            , "Consistency check for insertion at the end of vector");
     }
 
 
